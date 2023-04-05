b050ea331c4ca8f396ef47811037d95df6f67431
Nicolas BLONDEAU <niblondeau>
2023-04-05 11:58:21 +0200

    Ajout de la classe Monstre, et tests de la classe dans la console -> FONCTIONNEL :white_check_mark:

---
+++ Console/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Modèle;
+using System;
+using System.Reflection.PortableExecutable;
+
+//Création non permanente d'une liste de caractéristiques (pour tests) -> PEUT-ÊTRE EN FAIRE UNE CLASSE PLUS TARD???
+List<string> charact = new List<string>();
+charact.AddRange(new List<string> { "Caractéristique 1", "C2", "C3", "ENCORE UNE CARACTÉRISTIQUE", "Again and again" });
+
+//Création non permanente d'une liste d'apparences (pour tests) -> PEUT-ÊTRE EN FAIRE UNE CLASSE PLUS TARD AUSSI???
+List<string> appear = new List<string>();
+appear.AddRange(new List<string> { "Wow1", "Wow2", "Wow3", "ENCORE UNE APPARENCE" });
+
+Monstre monstre = new Monstre ( 1, "Warden", "Le Warden est une entité conçue pour vous traquer. Il est comme un GPS vivant capable d'optimiser ses déplacements pour vous contrer," +
+    " sachant que s'il n'arrive pas à vous coincer, il pourra toujours utiliser son attaque... [Plus]", charact, appear);
+Console.WriteLine(monstre.IntroduceTest());
+Console.WriteLine();
+Console.WriteLine("Liste de mes caractéristiques :");
+monstre.CharacteristicsList.ForEach(Console.WriteLine);
+Console.WriteLine();
+Console.WriteLine("Liste de mes apparences :");
+monstre.AppearanceList.ForEach(Console.WriteLine);
+Console.WriteLine();
+Console.WriteLine();
+Monstre monstre2 = new Monstre(423, "Mouton", "Je suis un animal présent dans la campagne.", charact, appear);
+Console.WriteLine(monstre2.IntroduceTest());
---
+++ Modèle/Monstre.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using System.Reflection.Metadata;
+
+namespace Modèle;
+
+public class Monstre
+{
+    public int Id { get; set; } = 1;
+    public string Name { get; set; }
+    public string Description { get; set; } = string.Empty;
+    
+    private List<string> characteristic;
+    public List<string> CharacteristicsList
+    {
+        get {
+            return characteristic;
+        }
+        set {
+            characteristic = value;
+        }
+    }
+    
+    private List<string> appearance;
+    public List<string> AppearanceList
+    {
+        get
+        {
+            return appearance;
+        }
+        set
+        {
+            appearance = value;
+        }
+    }
+
+    public string IntroduceTest()
+    {
+        return $"Je suis un {Name} (id : {Id}). Description : {Description}";
+    }
+    public Monstre(int id, string name, string desc, List<string> characList, List<string> appearList)
+    {
+        Id = id;
+        Name = name;
+        Description = desc;
+        CharacteristicsList = characList;
+        AppearanceList = appearList;
+    }
+}
